009c553ef33eba157ea6f9fec1602536c8aeac90
Bhabha September <141716338+BhabhaSeptember@users.noreply.github.com>
2025-07-11 13:31:42 +0200

    tut03

---
+++ 03-runnerz/src/main/java/runnerz/run/RunNotFoundException.java
@@ -0,0 +1,14 @@
+package runnerz.run;
+
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class RunNotFoundException extends RuntimeException{
+	
+	
+	public RunNotFoundException() {
+		super("Run Not Found");
+	}
+
+}
